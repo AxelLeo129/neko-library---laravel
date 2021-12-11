090e7a6b7d1e7490fa26f78e9ab23bf44608a719
AxelLeo129 <axelleo129dev@gmail.com>
2021-12-11 01:30:51 -0600

    Fixing products endpoint.

--- app/Http/Controllers/Prodotto/ProdottoController.php
+++ app/Http/Controllers/Prodotto/ProdottoController.php
@@ -13,7 +13,8 @@ class ProdottoController extends Controller
             $ordinato_da = $request->query('order_by');
             $campo_ricerca = $request->query('search_field');
             $valore_ricerca = $request->query('search_value');
-            $prodotti = empty($request->query('order_by')) ? Prodotto::orderBy($ordinato_da, 'desc')->paginate(4) : Prodotto::orderBy($ordinato_da, 'desc')->where($campo_ricerca, $valore_ricerca)->paginate(4);
+            $limite = $request->query('limit');
+            $prodotti = empty($request->query('order_by')) ? Prodotto::orderBy($ordinato_da, 'desc')->paginate($limite) : Prodotto::orderBy($ordinato_da, 'desc')->where($campo_ricerca, $valore_ricerca)->paginate($limite);
             return response($prodotti, 200);
         } catch (\Exception $e) {
             return response(['result' => 'fail', 'message' => $e->getMessage()], 500);
