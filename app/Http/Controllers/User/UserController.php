c2238a0cd84223add989a05afa923c3f571db98d
AxelLeo129 <axelleo129dev@gmail.com>
2022-08-15 17:41:48 -0600

    Register

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -18,4 +21,32 @@ class UserController extends Controller
         $access_token = Auth::user()->createToken('auth_neko_library')->accessToken;
         return response(["usuario" => Auth::user(), "access_token" => $access_token], 200);
     }
+
+    public function register(Request $request) {
+        $validar = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        if ($validar->fails()) {
+            return response(['result' => 'fail', 'message' => $validar->errors()], 500);
+        }
+
+        $usuario = new User();
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = Hash::make($request->password);
+        if(!$usuario->save()) {
+            return response(['result' => 'fail', 'message' => 'Error al crear usuario, por favor inténtelo más tarde.'], 500);
+        }
+        $access_token = $usuario->createToken('authToken')->accessToken;
+    
+        return response([
+            'result' => 'success', 
+            "message"=> 'Usuario creado exitósamente.',
+            'access_token' => $access_token
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -15,15 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login', 'App\Http\Controllers\User\UserController@login');
+Route::post('/register', 'App\Http\Controllers\User\UserController@register');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
 
 Route::get('template', 'App\Http\Controllers\Modello\ModelloController@get');
 Route::get('categorias-subcategorias', 'App\Http\Controllers\Categoria\CategoriaController@listCategoriesAndSubcategories');
 Route::get('verificar-categoria/{ruta}', 'App\Http\Controllers\Categoria\CategoriaController@verifyRouteCategory');
 Route::get('verificar-subcategoria/{ruta}', 'App\Http\Controllers\Categoria\CategoriaController@verifyRouteSubcategory');
 Route::get('slider', 'App\Http\Controllers\Cursore\CursoreController@list');
-Route::get('products', 'App\Http\Controllers\Prodotto\ProdottoController@list');
+Route::get('products', 'App\Http\Controllers\Prodotto\ProdottoController@list')->middleware('auth:api');
 Route::get('verificar-producto/{ruta}', 'App\Http\Controllers\Prodotto\ProdottoController@verifyRoute');
